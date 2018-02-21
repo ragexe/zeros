a802e2992e65965eadb3f0a36e420f02a55cbb8a
ragexe <ragexe@mail.ru>
2018-02-22 02:03:36 +0300

    zeros/ final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+
+    let result = 0;
+    let power = 1;
+    let poweredMultiplier = 5 ** power;
+
+    while (number >= poweredMultiplier){
+        number = number - number % poweredMultiplier;
+        result += number / poweredMultiplier;
+        poweredMultiplier = 5 ** ++power;
+    }
+
+    return result
+};
